370ccdeaa1c2e358fa9f170caeb087e2e58d5d72
Felipe Cristovão <felipecss88@gmail.com>
2021-11-11 16:13:04 -0300

    Estilos globais finalizados

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,8 @@ import { GlobalStyle } from './styles/global';
 
 const Title=styled.h1`
   
-color:black;
-font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
-font-weight:bolder;
-font-style:italic;
-font-size:100px;
+
+
 
 `
 export function App() {
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,17 +3,48 @@ import {createGlobalStyle} from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
 
 :root {
-    --background:gold;
+    --background:#F0F2F5;
+    --blue:#5429CC;
+    --green:#33CC95;
+    --red:#E62E4D;
+    --blue-light:#6933FF;
+    --text-title:#363F5F;
+    --text-body:#969CB3;
+    --shape:#FFFFFF;
 }
 
 *{
     margin:0;
     padding:0;
     box-sizing:border-box;
+   
+    
+}
+
+html{
+    
+    @media (max-width: 1080px){
+        font-size:93.75%;
+        
+
+    }
+    @media (max-width:720px){
+        font-size:87.5%;
+        
+
+    }
+
 }
 body{
 
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+}button{
+    cursor:pointer;
 }
-`
+
+[disabled]{
+    opacity: 0.6;
+    cursor:not-allowed;
+
+}`
