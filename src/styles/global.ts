d016685f64d422f272b08ecfe95d8974896d0d6b
Felipe Cristovão <felipecss88@gmail.com>
2021-11-11 17:03:28 -0300

    fontes do google adicionadas

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -39,7 +39,20 @@ body{
 
     background: var(--background);
     -webkit-font-smoothing: antialiased;
-}button{
+}
+
+body , input , textarea , button {
+
+    font-family:'Poppins',sans-serif;
+    font-weight:400;
+}
+
+h1 , h2 , h3 , h4 , h5 ,h6 , strong {
+
+    font-weight:600;
+}
+
+button{
     cursor:pointer;
 }
 
