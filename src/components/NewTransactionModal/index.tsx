c186411ccb39c29851cb42d6899835dbc0ad9753
Felipe <felipecss88@gmail.com>
2021-11-15 17:29:35 -0300

    Modal estruturado

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+import { Container } from './style';
+
 
 interface NewTrasactionModalProps {
 
@@ -9,11 +11,22 @@ interface NewTrasactionModalProps {
 export function NewTransactionModal({isOpen,onRequestClose}:NewTrasactionModalProps){
     return(
 
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}
    
-    >
+        <Modal 
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+         >
+        <Container>
+      <h2>Cadastrar Transação</h2>      
+       <input placeholder="Titulo"></input>
+       <input type="Number" placeholder="Preço"></input>
+       <input placeholder="Categoria"/>
+       <button type="submit">Cadastrar</button>
 
-  <p>Transaçôes</p>
+       </Container>
    </Modal>
+ 
     )
 }
