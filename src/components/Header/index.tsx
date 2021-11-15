bcdaf458227115fd3ce487f8361652751796c459
Felipe <felipecss88@gmail.com>
2021-11-15 09:31:33 -0300

    Componente Modal criado no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,32 @@
 
-//import { Component } from 'react'
+import {useState}from 'react'
 import logo from '../../Assets/Logo.svg'
 import { Container, Contant } from './style'
+import Modal from 'react-modal';
 
 export function Header(){
+
+    const[isOpenModalTransaction,setisOpenModalTransaction]=useState(false)
+
+    function handleOpenModal(){
+
+         setisOpenModalTransaction(true)
+    }
+    function handleCloseModal(){
+
+       setisOpenModalTransaction(false)
+    }
 
     return(
        <Container>
            <Contant>
            <img src={logo} alt="Logo" />
-            <button>Nova Transação</button>
+            <button type="button" onClick={handleOpenModal}>Nova Transação</button>
            </Contant>
-           
+          <Modal isOpen={isOpenModalTransaction} onRequestClose={handleCloseModal}>
+
+              <p>Transaçôes</p>
+          </Modal>
        </Container>
             
         
